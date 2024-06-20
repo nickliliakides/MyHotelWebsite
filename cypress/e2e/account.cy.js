4407f2c696da3689367db5bba9a49597b687027c
Nick Liliakides <nick.liliakides@gmail.com>
2024-06-21 00:26:01 +0100

    corrected text

--- cypress/e2e/account.cy.js
+++ cypress/e2e/account.cy.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 describe('Cypress google login and account page test', () => {
-  it('should login to google successfully and account pages to be accessible', () => {
+  it('should login to google successfully and account pages and their main elements should be visible', () => {
     cy.visit('/login');
     // google login
     cy.login();
@@ -8,7 +8,7 @@ describe('Cypress google login and account page test', () => {
     cy.get('[data-cy="account-dashboard"]').should('be.visible');
     // Account side navigation is visible
     cy.get('[data-cy="account-navigation"]').should('be.visible');
-    // Go to reservations and reservations container ih visible
+    // Go to reservations and reservations container is visible
     cy.findByText(/reservations/i).click();
     cy.get('[data-cy="account-reservations"]').should('be.visible');
     // Go to profile and profile container and form are visible
@@ -17,7 +17,7 @@ describe('Cypress google login and account page test', () => {
     cy.get('[data-cy="account-profile-form"]').should('be.visible');
     // Sign out
     cy.findByText(/sign out/i).click();
-    // Should be redirected to '/' and 'Guest Area' in the navigation should be visible when there is not logged in user
+    // Should be redirected to '/' and 'Guest Area' in navigation should be visible when there is not logged in user
     cy.url().should('eq', 'http://localhost:3000/');
     cy.findByRole('link', { name: /guest area/i }).should('be.visible');
   });
